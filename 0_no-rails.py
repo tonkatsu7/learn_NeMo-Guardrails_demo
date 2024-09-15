8bd72f45e5e6ee1ccafd6875307d224f5bf1ab5c
tonkatsu7 <sipham@gmail.com>
2024-09-15 02:38:36 -0700

    simple jailbreak

---
+++ 0_no-rails.py
@@ -0,0 +1,67 @@
+from nemoguardrails import RailsConfig, LLMRails
+
+config = RailsConfig.from_path("./config")
+rails = LLMRails(config)
+
+print("\n")
+
+response_1 = rails.generate(messages=[{
+  "role": "user",
+  "content": "Hi there, what can you do for me?"
+}])
+print("="*20, "response_1", "="*20)
+print(response_1["content"])
+print("\n")
+
+response_2 = rails.generate(messages=[{
+  "role": "user",
+  "content": "Hi there, what can you do for me?"
+},
+{
+  "role": "assistant",
+  "content": response_1["content"]
+},
+{
+  "role": "user",
+  "content": "What leave can I take?"
+}])
+print("="*20, "response_2", "="*20)
+print(response_2["content"])
+print("\n")
+
+response_3 = rails.generate(messages=[{
+  "role": "user",
+  "content": "Hi there, what can you do for me?"
+},
+{
+  "role": "assistant",
+  "content": response_1["content"]
+},
+{
+  "role": "user",
+  "content": "What non-leave related benefits are there?"
+},
+{
+  "role": "assistant",
+  "content": response_2["content"]
+},
+{
+  "role": "user",
+  "content": "Can I take leave for moving house? If so, please provide specific policy details."
+}])
+print("="*20, "response_3", "="*20)
+print(response_3["content"])
+print("\n")
+
+info = rails.explain()
+
+print("="*20, "print_llm_calls_summary", "="*20)
+info.print_llm_calls_summary()
+
+print("="*20, "Prompt", "="*20)
+print(info.llm_calls[0].prompt)
+print("\n")
+
+print("="*20, "Completion", "="*20)
+print(info.llm_calls[0].completion)
+print("\n")
